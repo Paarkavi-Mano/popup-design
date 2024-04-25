1e06cf449f4612e51e1be7fe7255e4bdb074c1b9
Paarkavi-Mano <paarkavimanoharan@gmail.com>
2024-04-25 16:42:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let popupElement = document.getElementById("popup");
+
+function openPopup(){
+    popupElement.classList.add("open-popup")
+}
+
+function closePopup(){
+    popupElement.classList.remove("open-popup")
+}
+
